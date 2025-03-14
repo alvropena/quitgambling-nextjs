3e59317eb2a4571223ac63793fb357bec49aeae1
Alvaro Peña <me@alvropena.com>
2025-03-14 08:05:14 -0700

    Add new pages for Blog, Privacy Policy, Support, and Terms

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <div className="container mx-auto max-w-6xl px-4 py-8 md:px-6">
+      <h1 className="text-4xl font-bold tracking-tight">Blog</h1>
+    </div>
+  )
+}
---
+++ app/support/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+    return (
+      <div className="container mx-auto max-w-6xl px-4 py-8 md:px-6">
+        <h1 className="text-4xl font-bold tracking-tight">Support</h1>
+      </div>
+    )
+  }
+  
